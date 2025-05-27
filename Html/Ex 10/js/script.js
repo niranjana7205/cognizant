08a3120c554ff05cb5b6c6dc8ababf064f58bfd6
niranjana7205 <niranjana7205@gmail.com>
2025-05-27 17:33:48 +0530

    html

---
+++ Html/Ex 10/js/script.js
@@ -0,0 +1,8 @@
+function calculateTotal() {
+    let price = 50;
+    let quantity = 3;
+    let total = price * quantity;
+  
+    console.log("Total is:", total);  // Set a breakpoint here
+  }
+  
